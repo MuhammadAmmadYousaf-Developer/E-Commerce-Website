eb403b35ccb17bd0e45124b83c1eab76ad0390d3
MuhammadAmmadYousaf-Developer <151757876+MuhammadAmmadYousaf-Developer@users.noreply.github.com>
2023-11-23 02:42:50 +0500

    let see if the data has been added on the project or not

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = new express();
+
+app.get(
+    "/",
+    (req,res)=>{
+        res.send("Hello, we are here! and this is entered after the code is compile and running to see if the nodemon is working on runtime or not or it crashes or not let see if this all get to the main page of the website on port 5000. This is the text send after the original text and checked working and also tested on mobile not sure how the out put looks like but we will see what is going on!")
+    }
+)
+
+app.listen(
+    "5000",
+    ()=>console.log("Background is running")
+)
